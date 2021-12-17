ff5b354747a2ea4023f03d622ddd87d3f6ba082c
divineajaegbu <divineajaegbu4@gmail.com>
2021-12-17 22:35:43 +0100

    turing-website

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// const pickable = document.getElementById("test")
+// if (pickable) {
+//     pickable.style.color = "red"
+//     pickable.style.background = "green";
+// }
+
+
+// const search = document.querySelector("click-appear1");
+// const site = document.querySelector("click-appear");
+// const flexible = document.getElementById("scope");
+// const freecate = document.getElementById("clone");
+// const color = document.querySelector("search");
+
+
+
+
+
+// const picker = () => {
+//     flexible.addEventListener("click", function() {
+//         site.style.display = "block";
+//     })
+// }
+
+// picker();
---
+++ js/solving.js
@@ -0,0 +1,7 @@
+const solveProblems = document.getElementById("scope");
+const deliver = document.getElementById("clone");
+const clickAppear = document.querySelector("click-appear");
+const clickAppear1 = document.querySelector("click-appear1");
+
+
+clickAppear.style.background = "red";
